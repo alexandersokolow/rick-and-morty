0b32bbe8846f6afcb768cbaf32b3ee8700e4bbf2
alexandersokolow <alexandersokolow96@gmail.com>
2022-06-04 13:22:10 +0200

    added basic List component

---
+++ src/components/List.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Boilerplate.css";
+import useModels from '../hooks/useModels';
+
+interface Props {
+  model: "characters" | "locations" | "episodes";
+}
+
+export const List = ({model}: Props) => {
+  const { loading, error, data } = useModels(1, model);
+  console.log("loading: ", loading);
+  console.log("error: ", error);
+  console.log("data: ", data);
+
+  return (<div></div>);
+};
