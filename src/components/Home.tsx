0e9b35462d60d3880c715c621cf5e4929ff2623b
alexandersokolow <alexandersokolow96@gmail.com>
2022-06-04 22:53:11 +0200

    added logo to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import styles from "../styles/Home.module.css";
 import { Link } from "react-router-dom";
+import Logo from '../logo.png';
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1>Rick and Morty Lists</h1>
+      <img src={Logo} className={styles.logo} alt="" />
+      <h1>Rick and Morty Database</h1>
       <Link to="/characters" className={styles.link}>Characters</Link>
       <Link to="/locations" className={styles.link}>Locations</Link>
       <Link to="/episodes" className={styles.link}>Episodes</Link>
