a0a75a697a2679521711bd1cdd0820b4222b0950
alexandersokolow <alexandersokolow96@gmail.com>
2022-06-04 13:23:36 +0200

    removed unnecessary imports

--- src/Boilerplate.tsx
+++ src/Boilerplate.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Boilerplate.css";
 import useCharacters from './hooks/useCharacters';
 
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Boilerplate.css";
 import useModels from '../hooks/useModels';
 
