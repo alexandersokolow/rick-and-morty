dcb753d16f6de65a7f1dfabc2387559dfd62cd2c
alexandersokolow <alexandersokolow96@gmail.com>
2022-06-08 17:37:21 +0200

    added basic apollo mock & removed obsolete test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,62 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { App } from "./App";
 import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import { gql } from '@apollo/client';
 
-const mocks: MockedResponse[] = [];
+export const GET_CHARACTERS_QUERY = gql`
+  query {
+    characters(page: 1) {
+      info {
+        count,
+        pages
+      }
+      results {
+        id
+        name
+        species
+        origin {
+          name
+        }
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
 
-test("renders boilerplate", () => {
+const mocks: MockedResponse[] = [
+ {
+    request: {
+      query: GET_CHARACTERS_QUERY
+    },
+    result: {
+      data: {
+        characters: {
+          info: {
+            count: 1,
+            pages: 1,
+          },
+          results: [
+            {
+              id: 1,
+              name: "test",
+              species: "test",
+              origin: { name: "test" },
+              location: { name: "test" },
+            }
+
+          ]
+        },
+      }
+    },
+  },
+];
+
+test("renders app", () => {
   render(
     <MockedProvider mocks={mocks} addTypename={false}>
       <App />
     </MockedProvider>
   );
-  const header = screen.getByRole("heading", {
-    name: /full stack web engineer/i,
-  });
-  expect(header).toBeInTheDocument();
 });
